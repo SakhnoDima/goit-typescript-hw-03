fb2be9719facee1eb71402bfdf8dcf8b25c18ab9
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-09-14 17:05:18 +0300

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,53 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.floor(Math.random() * 10);
+  }
+  getSignature(): number {
+    console.log(this.signature);
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key, private name: string) {}
+  getKey(): Key {
+    console.log(this.key);
+    return this.key;
+  }
+  getName(): string {
+    return this.name;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public tenants: Person[] = [];
+
+  constructor(public key: Key) {}
+
+  comeIn(person: Person): Person | undefined {
+    if (!this.door) return;
+    this.tenants.push(person);
+    console.log(`Welcome home ${person.getName()}`);
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: object): void {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
-
 const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person(key, "Stich");
 
+key.getSignature();
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
-
-export {};
+export { Key, MyHouse, Person };
